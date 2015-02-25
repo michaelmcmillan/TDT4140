3c054d537110fb2ff8e5b57910186e44dcb7df6a
Michael McMillan <email@michaelmcmillan.net>
2015-02-25 12:57:11 +0100

    Added Lasses changes manually.

--- src/controllers/LoginViewController.java
+++ src/controllers/LoginViewController.java
@@ -1,5 +1,43 @@
 package controllers;
 
-public class LoginViewController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+public class LoginViewController{
+    private final String MAINVIEW_PATH = "MainView.fxml";
+
+    public void fireLoginButton(ActionEvent event) throws  Exception{
+        Scene scene = ((Node)event.getTarget()).getScene();
+        Stage primaryStage = (Stage) scene.getWindow();
+
+        TextField usernameField = (TextField) scene.lookup("#usernameTextfield");
+        TextField passwordField = (TextField) scene.lookup("#passwordTextfield");
+
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if (username.equals("lqasse")){ //TODO implement check against database
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../views/"+MAINVIEW_PATH));
+            Parent main = (Parent) loader.load();
+            primaryStage.setTitle("Kalendersystem");
+            primaryStage.setScene(new Scene(main));
+
+
+        }
+
+    }
+    public void fireRegisterButton(ActionEvent event){
+
+    };
+
+
 
 }
--- src/controllers/MainViewController.java
+++ src/controllers/MainViewController.java
@@ -1,16 +1,11 @@
 package controllers;
 
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainViewController implements Initializable {
-
-    //@FXML private Pane dialog;
-    //@FXML private DayDetailViewController dayDetailViewController;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
