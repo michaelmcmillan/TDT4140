9078b7d720415a8f265d120f888ec60ea9a297e0
sharklaks <sharklaks@gmail.com>
2015-02-26 11:45:09 +0100

    Lagt til hovedfunksjoner og felter

--- src/models/Calendar.java
+++ src/models/Calendar.java
@@ -1,7 +1,39 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  * Created by sharklaks on 26/02/15.
  */
 public class Calendar {
+
+    ArrayList<Appointment> appointments;
+
+    public void addAppointment(Appointment appointment){
+        appointments.add(appointment);
+    }
+
+    public void removeAppointment(Appointment appointment){
+        if(appointments.contains(appointment))
+            appointments.remove(appointment);
+    }
+
+    public boolean overlaps(Appointment appointment1, Appointment appointment2){
+
+        //***MA ENDRES
+        return true;
+    }
+
+    public ArrayList<Appointment> getAppointmentsBetween(Date startTime, Date endTime){
+        ArrayList<Appointment> appointmentsBetween = new ArrayList<Appointment>();
+        for(int i = 0; i < appointments.size(); i++){
+            Appointment currentAppointment = appointments.get(i);
+
+            //if(startTime.before(currentAppointment.))
+
+        }
+        return appointmentsBetween;
+    }
+
 }
