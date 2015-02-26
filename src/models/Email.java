3bdbff8afd56d7d0ccfea572ea6437ecd5a64033
sharklaks <sharklaks@gmail.com>
2015-02-26 13:21:06 +0100

    La til Email-klasse

---
+++ src/models/Email.java
@@ -0,0 +1,12 @@
+package models;
+
+/**
+ * Created by sharklaks on 26/02/15.
+ */
+public class Email {
+
+    public static void send(String fromEmail, String toString, String subject, String body){
+        
+    }
+
+}
