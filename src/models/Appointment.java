57ce0f0e62317879257b93af6572d5f52c875466
sharklaks <sharklaks@gmail.com>
2015-02-26 11:12:53 +0100

    Lagt til hovedfunksjoner

--- src/models/Appointment.java
+++ src/models/Appointment.java
@@ -1,5 +1,6 @@
 package models;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -11,10 +12,31 @@ public class Appointment {
     Date endTime;
     String title;
     String description;
-    Person[] participants;
+    ArrayList<Person> participants;
     Person createdBy;
 
     public Appointment () {
 
+    }
+
+    public void addParticipant(Person person){
+        participants.add(person);
+    }
+
+    public void removeParticipant(Person person){
+        participants.remove(person);
+    }
+
+    public void changeTime(Date startTime, Date endTime){
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public void changeTitle(String title){
+        this.title = title;
+    }
+
+    public void changeDescription(String description){
+        this.description = description;
     }
 }
