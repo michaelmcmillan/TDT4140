5ae0a47c1e1d5644c5da2c2f65fe2e89e0d8f9d0
sharklaks <sharklaks@gmail.com>
2015-02-26 11:08:37 +0100

    Lagt til felter

--- src/models/Appointment.java
+++ src/models/Appointment.java
@@ -1,9 +1,18 @@
 package models;
 
+import java.util.Date;
+
 /**
  * Created by sharklaks on 26/02/15.
  */
 public class Appointment {
+
+    Date startTime;
+    Date endTime;
+    String title;
+    String description;
+    Person[] participants;
+    Person createdBy;
 
     public Appointment () {
 
