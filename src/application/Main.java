4f4d35a622b8f59be845a8f8043dc22371721556
Michael McMillan <email@michaelmcmillan.net>
2015-03-04 10:45:58 +0100

    Made the window a fixed size.

--- src/application/Main.java
+++ src/application/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
         primaryStage.setTitle("Login - Kalendersystem");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        primaryStage.setResizable(false);
     }
 
     public static void main(String[] args) {
