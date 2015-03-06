9fa25961bb8d91a3586d0911b8642d16e61c9d33
lqasse <ladrev@gmail.com>
2015-03-06 16:12:42 +0100

    LoginView handles Enterkey == loginbutton txt and pw field goes red if empty

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 
+import controllers.LoginViewController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,16 +8,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    private final String MAINVIEW_PATH = "MainView.fxml";
-    private final String LOGINVIEW_PATH = "LoginView.fxml";
+    private final String LOGINVIEW_PATH = "\"../views/LoginView.fxml";
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        new LoginViewController(primaryStage);
+        /*
         Parent root = FXMLLoader.load(getClass().getResource("../views/" + LOGINVIEW_PATH));
         primaryStage.setTitle("Login - Kalendersystem");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
         primaryStage.setResizable(false);
+        */
     }
 
     public static void main(String[] args) {
--- src/controllers/LoginViewController.java
+++ src/controllers/LoginViewController.java
@@ -1,34 +1,110 @@
 package controllers;
 
 import javafx.event.ActionEvent;
-import javafx.scene.Node;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import models.Person;
 
 
 public class LoginViewController{
+    private Stage primaryStage;
+    private Scene scene;
+    private final String LOGINVIEW_PATH = "../views/LoginView.fxml";
+    TextField usernameField;
+    TextField passwordField;
+    Button loginButton;
+    Button registerButton;
+
+    public LoginViewController(Stage primaryStage) throws Exception{
+        this.primaryStage = primaryStage;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(LOGINVIEW_PATH));
+        Parent root = loader.load();
+        loader.setController(this);
+        primaryStage.setTitle("Login - Kalendersystem");
+        scene = new Scene(root);
+        primaryStage.setScene(scene);
+
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+        usernameField = (TextField) scene.lookup("#usernameTextfield");
+        passwordField = (TextField) scene.lookup("#passwordTextfield");
+        loginButton = (Button) scene.lookup("#loginButton");
+        registerButton = (Button) scene.lookup("#registerButton");
+
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.ENTER) {
+
+                    doLogin();
+
+                }
+
+            }
+        });
+
+        loginButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                doLogin();
+            }
+        });
+
+
+
+    }
+
+
     private final String MAINVIEW_PATH = "MainView.fxml";
 
-    public void fireLoginButton(ActionEvent event) throws  Exception{
-        Scene scene = ((Node)event.getTarget()).getScene();
-        Stage primaryStage = (Stage) scene.getWindow();
+    public void doLogin() {
 
-        TextField usernameField = (TextField) scene.lookup("#usernameTextfield");
-        TextField passwordField = (TextField) scene.lookup("#passwordTextfield");
+        try {
 
-        String username = usernameField.getText();
-        String password = passwordField.getText();
-
-        // Add a new appointment to the calendar based on input times
-        Person user = new Person(username);
-//        final Appointment appointment = new Appointment(startDate, endDate, "Yolo", "Some awesome stuff is happening here", morten);
+            String username = usernameField.getText();
+            String password = passwordField.getText();
+            if (!username.equals("") && !password.equals("")){
+                // Add a new appointment to the calendar based on input times
+                Person user = new Person(username);
+//         final Appointment appointment = new Appointment(startDate, endDate, "Yolo", "Some awesome stuff is happening here", morten);
 //        calendar.addAppointment(appointment);
 
-        if (true){ //TODO implement check against database
-            new MainViewController(primaryStage, user);
+                if (true){ //TODO implement check against database
+                    new MainViewController(primaryStage, user);
+                }
+            } else {
+
+                if (username.equals("")){
+                    usernameField.setStyle("-fx-background-color: red");
+                } else {
+                    usernameField.setStyle("-fx-background-color: white");
+                }
+
+                if (password.equals("")){
+                    passwordField.setStyle("-fx-background-color: red");
+                } else {
+                    usernameField.setStyle("-fx-background-color: white");
+                }
+
+            }
+
+
+
+        } catch (Exception e){
+
         }
+
+
+
     }
 
     public void fireRegisterButton(ActionEvent event){
