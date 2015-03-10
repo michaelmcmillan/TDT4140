c70bbeebbc6dcffde45fb9587496b075fd9dbf7a
Michael McMillan <email@michaelmcmillan.net>
2015-03-10 15:28:21 +0100

    Server is now a Singleton which can be accessed from anywhere. Also, the serverHTTPCommunicator now checks for response codes which are invalid.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,20 +1,26 @@
 package application;
 
 import controllers.LoginViewController;
-import server.ServerMethods;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import server.Server;
 
 public class Main extends Application {
     private final String LOGINVIEW_PATH = "\"../views/LoginView.fxml";
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        new LoginViewController(primaryStage); 
-
+        new LoginViewController(primaryStage);
     }
 
     public static void main(String[] args) {
+
+        Server.getInstance().logInAs("mcmillan@live.com", "heisann");
+
+        if (Server.getInstance().isAuthenticated()) {
+            System.out.println("Du er innlogget!");
+        }
+
         launch(args);
     }
 }
