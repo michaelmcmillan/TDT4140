7f0ca22c294a11ac4ae70507aa5dffb7ce2beea2
Morten Kleveland <mortengkleveland@gmail.com>
2015-03-12 11:00:48 +0100

    EditGroup controller now inherits from Group Controller

--- src/controllers/EditGroupPopupViewController.java
+++ src/controllers/EditGroupPopupViewController.java
@@ -1,92 +1,11 @@
 package controllers;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
+
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Rectangle;
-import models.Appointment;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.ResourceBundle;
+public class EditGroupPopupViewController extends GroupPopupViewController {
 
-
-public class EditGroupPopupViewController implements Initializable {
-
-    @FXML private TextField titleTextField;
-    private ArrayList<Pane> openGroupPopups = new ArrayList<Pane>();
-    private ArrayList<Rectangle> rectangles = new ArrayList<Rectangle>();
-    private Appointment model;
-    private Pane calendarPane;
-
-    public EditGroupPopupViewController(Pane calendarPane){
-        this.calendarPane = calendarPane;
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        model = new Appointment();
-    }
-
-    @FXML void addGroupButtonPressed(ActionEvent event) {
-
-    }
-
-    public void show(){
-
-        try {
-            // Init popupview from FXML
-            FXMLLoader testLoader = new FXMLLoader(getClass().getResource("../views/EditGroupPopupView.fxml"));
-            Pane groupPopup = testLoader.load();
-            groupPopup.setId("groupPopup");
-
-            // Get controller, add view to main view
-            //AppointmentPopupViewController appointmentPopupViewController = testLoader.getController();
-            calendarPane.getChildren().add(groupPopup);
-            openGroupPopups.add(groupPopup);
-
-            // Set popup to center position FIX!
-            double appointmentPopupWidth = groupPopup.getWidth();
-            double appointmentPopupHeight = groupPopup.getHeight();
-            double mainPaneWidth = calendarPane.getLayoutX();
-            double mainPaneHeight = calendarPane.getLayoutY();
-            groupPopup.setLayoutX(mainPaneWidth/2 - appointmentPopupWidth/2);
-            groupPopup.setLayoutY(mainPaneHeight/2 - appointmentPopupHeight/2);
-
-            groupPopup.setLayoutX(80);
-            groupPopup.setLayoutY(100);
-
-            //Set methods
-            Button closeButton = (Button) groupPopup.lookup("#closeButton");
-            TextField startTime = (TextField) groupPopup.lookup("#startTime");
-            TextField endTime = (TextField) groupPopup.lookup("#endTime");
-            DatePicker appointmentDate = (DatePicker) groupPopup.lookup("#startDatePicker");
-
-            closeButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent event) {
-                    close();
-                }
-            });
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void close() {
-        for (int i = 0; i < calendarPane.getChildren().size(); i++) {
-            if (openGroupPopups.contains(calendarPane.getChildren().get(i))) {
-                calendarPane.getChildren().remove(calendarPane.getChildren().get(i));
-            }
-        }
+    public EditGroupPopupViewController(Pane calendarPane) {
+        super(calendarPane);
     }
 }
