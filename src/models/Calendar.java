a1b3fe17d2abe16cb121df45269959d90a01ae7e
Michael McMillan <email@michaelmcmillan.net>
2015-03-12 13:34:21 +0100

    Removed old signature.

--- src/models/Calendar.java
+++ src/models/Calendar.java
@@ -3,9 +3,6 @@ package models;
 import java.util.ArrayList;
 import java.util.Date;
 
-/**
- * Created by sharklaks on 26/02/15.
- */
 public class Calendar {
 
     private String calendarTitle;
@@ -27,42 +24,4 @@ public class Calendar {
         if(appointments.contains(appointment))
             appointments.remove(appointment);
     }
-
-    /*public boolean overlaps(Appointment appointment1, Appointment appointment2){
-        Date s1 = appointment1.getStartTime();
-        Date s2 = appointment2.getStartTime();
-        Date e1 = appointment1.getEndTime();
-        Date e2 = appointment2.getEndTime();
-
-        if(s2.after(s1) && s2.before(e1)){
-            return true;
-        }
-
-        if(e2.after(s1) && e2.before(e1)){
-            return true;
-        }
-
-        return false;
-    }
-
-    public ArrayList<Appointment> getAppointmentsBetween(Date startTime, Date endTime){
-        ArrayList<Appointment> appointmentsBetween = new ArrayList<Appointment>();
-        for(int i = 0; i < appointments.size(); i++){
-            Appointment currentAppointment = appointments.get(i);
-
-            if (startTime.before(currentAppointment.getStartTime()) && endTime.after(currentAppointment.getEndTime())) {
-                appointmentsBetween.add(currentAppointment);
-            }
-        }
-        return appointmentsBetween;
-    }
-
-    public ArrayList<Appointment> getAppointments() {
-        return this.appointments;
-    }
-
-    public String toString() {
-        return this.calendarTitle;
-    }
-    */
 }
