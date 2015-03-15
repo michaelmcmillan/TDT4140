acbc601c723bcde5c072b4336d2e095b4749fcb6
lqasse <ladrev@gmail.com>
2015-03-15 19:13:03 +0100

    la til felt for appointment i appointmentview

--- src/views/AppointmentView.java
+++ src/views/AppointmentView.java
@@ -1,14 +1,17 @@
 package views;
 
 import javafx.scene.shape.Rectangle;
+import models.Appointment;
 
 /**
  * Created by Morten on 27.02.15.
  */
 public class AppointmentView extends Rectangle {
+    private Appointment appointment;
     private boolean isClicked = false;
     public AppointmentView() {
         super();
+
     }
 
     public void setClicked(boolean isClicked) {
@@ -17,5 +20,13 @@ public class AppointmentView extends Rectangle {
 
     public boolean isClicked() {
         return isClicked;
+    }
+
+    public Appointment getAppointment(){
+        return this.appointment;
+    }
+
+    public void setAppointment(Appointment appointment) {
+        this.appointment = appointment;
     }
 }
