1d715f33ebf9a0c38eac1318d6f610e27c8ee8db
Morten Kleveland <mortengkleveland@gmail.com>
2015-03-15 18:25:56 +0100

    Registerview is now reachable from Loginview

--- src/controllers/RegisterViewController.java
+++ src/controllers/RegisterViewController.java
@@ -1,11 +1,16 @@
 package controllers;
 
-import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -13,6 +18,9 @@ public class RegisterViewController implements Initializable {
 
     private final String REGISTERVIEW_PATH = "../views/RegisterView.fxml";
     private Stage primaryStage;
+    private Button registerButton;
+    private Button cancelButton;
+    @FXML TextField emailField, passwordField, firstnameField, surnameField;
 
     public RegisterViewController(Stage primaryStage) throws Exception {
 
@@ -24,14 +32,36 @@ public class RegisterViewController implements Initializable {
         Scene scene = new Scene(main);
         scene.getStylesheets().add(this.getClass().getResource("/views/style.css").toExternalForm());
         primaryStage.setScene(scene);
-    }
 
-    void fireCancel(ActionEvent event) {
-        new LoginViewController(primaryStage);
-    }
+        // Get buttons and textviews
+        registerButton = (Button) scene.lookup("#registerButton");
+        cancelButton = (Button) scene.lookup("#cancelButton");
 
-    void fireRegister(ActionEvent event) {
-        new LoginViewController(primaryStage);
+        registerButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+
+                // TODO: Make a Server.getInstance().addPerson(person) method
+                // TODO: Validation.....
+                /*
+                int id = 123; // TODO: Overload Person constructur without ID?
+                Person person = new Person(id, emailField.getText(), firstnameField.getText(), surnameField.getText());
+                ArrayList persons = Server.getInstance().getAllUsers();
+                if (!persons.contains(person)) {
+                    Server.getInstance().addPerson(person);
+                }
+                */
+
+                new LoginViewController(primaryStage);
+            }
+        });
+
+        cancelButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                new LoginViewController(primaryStage);
+            }
+        });
     }
 
     @Override
