f7403c320e5c3a070e44ec02da832816ef775ddb
odni <odin1802@hotmail.com>
2015-03-17 15:01:39 +0100

    lagt til stylesheet

--- src/controllers/CalendarViewController.java
+++ src/controllers/CalendarViewController.java
@@ -47,7 +47,7 @@ public class CalendarViewController implements Initializable {
     private double startX, startY, endX, endY;
     private ArrayList<DayView> dayPanes = new ArrayList<DayView>();
     private Rectangle rect;
-    private final double appointmentRectangleCornerRadius = 4;
+    private final double appointmentRectangleCornerRadius = 0;
     DropShadow dropShadow = new DropShadow(2,4.0,4.0,Color.BLACK);
     private java.util.Calendar startOfWeek;
     private AppointmentPopupViewController popupView;
